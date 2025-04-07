aa471d61a32b7852788d5e19c65f3e39e05efec9
haqueashraful <88mohammedhaque@gmail.com>
2025-04-07 22:17:40 +0600

    project section work

--- src/features/Home/Components/banner.tsx
+++ src/features/Home/Components/banner.tsx
@@ -1,50 +1,66 @@
 "use client";
 
 import { ArrowRight, Download } from "lucide-react";
-// import { TypeAnimation } from "react-type-animation";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-
 import image from "@/assets/nobg.png";
+import { useEffect } from "react";
+
+// Optional: Define a type for the repo if you want type safety
+interface Repo {
+  name: string;
+  description: string | null;
+  homepage: string | null;
+  language: string | null;
+}
 
 const Banner = () => {
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/haqueashraful/repos?per_page=100")
+      .then((res) => res.json())
+      .then((data: Repo[]) => {
+        console.log(data, "data");
+        const withHomepage = data
+        .filter((repo) => repo.homepage && repo.homepage.trim() !== "")
+        .map((repo) => ({
+          name: repo.name,
+          description: repo.description,
+          homepage: repo.homepage,
+          language: repo.language,
+        }));
+      
+
+        console.log(withHomepage, "withHomepage");
+      })
+      .catch((err) => console.error("Error fetching repos:", err));
+  }, []);
+
   return (
     <div className="py-28">
       <div className="w-full mx-auto gap-10 lg:gap-0">
         <div className="w-72 mx-auto">
           <Image
-          data-aos="zoom-in"
-          data-aos-delay="300"
-          data-aos-anchor-placement="top-center"
-            width={0}
-            height={0}
-            alt="ashraful"
+            data-aos="zoom-in"
+            data-aos-delay="300"
+            data-aos-anchor-placement="top-center"
             src={image}
+            alt="ashraful"
+            width={288} // or use layout="responsive" if desired
+            height={288}
             className="rounded-full shadow-rn dark:shadow-dark-rn"
           />
         </div>
-        <div className=" w-full space-y-5 py-6">
-          <h1 className="text-3xl"> Hi! I&apos;m Ashraful Haque. 👋🏻</h1>
-          {/* <div className="w-2/3"> */}
-          {/* <h1>
-          <TypeAnimation
-          className="text-[2.5rem] lg:!text-[4rem]"
-            sequence={[
-              "Frontend Developer",
-              1000,
-              "Full-stack Developer",
-              1000,
-              "Software Developer",       
-              1000,
-            ]}
-            style={{fontWeight: "bolder", color: "red" }}
-            repeat={Infinity}
-          />
-          </h1> */}
-
-          <h1 className=" text-3xl lg:text-8xl font-bold">
-            Creating impactful<br /> experiences on <br className=" lg:hidden" /> <span className=" text-[#b6bcc6] dark:text-[#4d99f7]">web design</span>
+        <div className="w-full space-y-5 py-6">
+          <h1 className="text-3xl">Hi! I&apos;m Ashraful Haque. 👋🏻</h1>
+          <h1 className="text-3xl lg:text-8xl font-bold">
+            Creating impactful
+            <br />
+            experiences on <br className="lg:hidden" />
+            <span className="text-[#b6bcc6] dark:text-[#4d99f7]">
+              web design
+            </span>
           </h1>
 
           <p className="py-6 text-lg">
@@ -52,14 +68,13 @@ const Banner = () => {
             innovative solutions that elevate brands and captivate audiences
             around the world.
           </p>
-          <div className=" flex flex-col-reverse lg:flex-row items-center gap-4">
+          <div className="flex flex-col-reverse lg:flex-row items-center gap-4">
             <Button className="flex">
               Connect with me
               <ArrowRight />
             </Button>
 
             <Link
-              // href="@/assets/ashraful-haque-fd-resume.pdf"
               href="https://drive.google.com/uc?export=download&id=1e7r-Yb6lI6QhtR0j5nHbs5fq8FEuh_8R"
               download="ashraful-resume.pdf"
             >
--- src/features/Home/Components/project-section.tsx
+++ src/features/Home/Components/project-section.tsx
@@ -1,7 +1,7 @@
 "use client";
 
+import React, { useEffect, useState } from "react";
 import SectionTitle from "@/components/section-title";
-import React from "react";
 import {
   Carousel,
   CarouselContent,
@@ -9,77 +9,262 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
-import Autoplay from "embla-carousel-autoplay"
-
+import Autoplay from "embla-carousel-autoplay";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
-
-import ProjectData from "@/utils/data.json";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import Thumbnail from "@/components/thumbnail";
+import { usePathname } from "next/navigation";
 
-
-interface itemType {
-  id: number;
-  title: string;
-  description: string;
-  liveLink: string;
-  codeLink: string;
+interface Repo {
+  name: string;
+  description: string | null;
+  homepage: string | null;
+  language: string | null;
+  created_at: string;
+  topics?: string[];
 }
 
 const ProjectSection = () => {
+  const pathName = usePathname();
+  const [myProject, setMyProject] = useState<Repo[]>([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/haqueashraful/repos?per_page=100")
+      .then((res) => res.json())
+      .then((data: Repo[]) => {
+        const withHomepage = data
+          .filter((repo) => repo.homepage && repo.homepage.trim() !== "")
+          .map((repo) => ({
+            name: repo.name.replace(/[-_]/g, " "),
+            description: repo.description,
+            homepage: repo.homepage,
+            language: repo.language,
+            topics: repo.topics,
+            created_at: repo.created_at,
+          }));
+
+        const sortedProjects = withHomepage.sort((a, b) => {
+          return (
+            new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
+          );
+        });
+
+        setMyProject(sortedProjects);
+      })
+      .catch((err) => console.error("Error fetching repos:", err));
+  }, []);
+
+  const renderCard = (item: Repo, index: number) => (
+    <Card
+      key={index}
+      className={`w-full border !overflow-hidden ${
+        pathName === "/project" ? "min-h-[900px] !shadow-none" : "h-full shadow-rn dark:shadow-dark-rn"
+      }`}    >
+      <CardContent className="flex min-h-[700px] flex-col space-y-4 !p-0 !dark:bg-background">
+        <Thumbnail url={item.homepage || ""} className="w-full min-h-[700px]" />
+      </CardContent>
+      <CardFooter className=" flex-col pt-5 space-y-5">
+        {pathName === "/project" ? (
+          <>
+            <div className="flex items-center justify-between gap-2 w-full">
+              <h3 className="text-xl uppercase font-semibold w-full">
+                {item.name}
+              </h3>
+              <div className="w-full !text-xl flex items-end justify-end gap-5">
+                <Button variant="link" className="px-0 py-0">
+                  <Link target="_blank" href={item.homepage || ""}>
+                    Live View
+                  </Link>
+                </Button>
+                <Button variant="link" className="px-0 py-0">
+                  <Link
+                    target="_blank"
+                    href={`https://github.com/haqueashraful/${item.name}`}
+                  >
+                    Code Link
+                  </Link>
+                </Button>
+              </div>
+            </div>
+            <p className="text-sm text-white text-center flex-grow">
+              {item.description || "No description available."}
+            </p>
+
+            <div className="flex flex-wrap gap-2">
+              {item.topics?.map((tool, index) => (
+                <span
+                  key={index}
+                  className="bg-gray-200 dark:bg-gray-800 px-2 py-1 rounded-md text-sm"
+                >
+                  {tool}
+                </span>
+              ))}
+            </div>
+          </>
+        ) : (
+          <>
+            <h3 className="text-xl uppercase font-semibold">{item.name}</h3>
+            <p className="text-sm text-white text-center flex-grow">
+              {item.description}
+            </p>
+
+            <div className="flex flex-wrap gap-2">
+              {item.topics?.map((tool, index) => (
+                <span
+                  key={index}
+                  className="bg-gray-200 dark:bg-gray-800 px-2 py-1 rounded-md text-sm"
+                >
+                  {tool}
+                </span>
+              ))}
+            </div>
+
+            {/* Ensuring buttons always stay at the bottom */}
+            <div className="w-full flex justify-between mt-auto">
+              <Button variant="link" className="px-0 py-0">
+                <Link target="_blank" href={item.homepage || ""}>
+                  Live View
+                </Link>
+              </Button>
+              <Button variant="link" className="px-0 py-0">
+                <Link
+                  target="_blank"
+                  href={`https://github.com/haqueashraful/${item.name}`}
+                >
+                  Code Link
+                </Link>
+              </Button>
+            </div>
+          </>
+        )}
+      </CardFooter>
+    </Card>
+  );
+
   return (
-    <div>
+    <div className="w-full px-4 md:px-8 lg:px0">
       <SectionTitle
         intro="My Projects"
         title="My Latest Works"
         description="Welcome to my web development portfolio! Explore a collection of projects showcasing my expertise in front-end development."
       />
 
-      <Carousel
-       plugins={[
-        Autoplay({
-          delay: 2000,
-        }),
-      ]}
-      className="w-full"
-      >
-        <CarouselContent className="-ml-1">
-          {ProjectData.map((item: itemType) => (
-            <CarouselItem key={item.id} className="pl-1 md:basis-1/2 xl:basis-1/3">
-              <div className="p-1 h-full">
-                <Card className="min-h-full shadow-rn dark:shadow-dark-rn border !overflow-hidden">
-                  <CardContent className="flex flex-col h-full space-y-4 !p-0">
-                    <Thumbnail url={item.liveLink} className="w-full" />
-                  </CardContent>
-                  <CardFooter className="flex-col pt-5 space-y-5">
-                  <h3 className="text-xl uppercase font-semibold">{item.title}</h3>
-                    <p className="text-sm text-gray-600 text-center flex-grow">{item.description}</p>
-
-                    {/* Ensuring buttons always stay at the bottom */}
-                    <div className="w-full flex justify-between mt-auto">
-                      <Button variant="link" className="px-0 py-0">
-                        <Link target="_blank" href={item?.liveLink}>
-                          Live View
-                        </Link>
-                      </Button>
-                      <Button variant="link" className="px-0 py-0">
-                        <Link target="_blank" href={item?.codeLink}>
-                          Code Link
-                        </Link>
-                      </Button>
-                    </div>
-                  </CardFooter>
-                </Card>
-              </div>
-            </CarouselItem>
+      {pathName === "/project" ? (
+        <div className="flex flex-col gap-10 relative">
+          {myProject.map((item, index) => (
+            <div
+              key={index}
+              className="w-full sticky top-0"
+              //  style={{ top: `${index * 30}px`, right: `${index * 10}px` }}
+            >
+              {renderCard(item, index)}
+            </div>
           ))}
-        </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
-      </Carousel>
+        </div>
+      ) : (
+        <Carousel plugins={[Autoplay({ delay: 2000 })]} className="w-full">
+          <CarouselContent className="-ml-1">
+            {myProject.map((item, index) => (
+              <CarouselItem
+                key={index}
+                className="pl-1 md:basis-1/2 xl:basis-1/3"
+              >
+                <div className="p-1 h-full">{renderCard(item, index)}</div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      )}
     </div>
   );
 };
 
 export default ProjectSection;
+
+// "use client";
+
+// import SectionTitle from "@/components/section-title";
+// import React from "react";
+// import {
+//   Carousel,
+//   CarouselContent,
+//   CarouselItem,
+//   CarouselNext,
+//   CarouselPrevious,
+// } from "@/components/ui/carousel";
+// import Autoplay from "embla-carousel-autoplay"
+
+// import { Card, CardContent, CardFooter } from "@/components/ui/card";
+
+// import ProjectData from "@/utils/data.json";
+// import { Button } from "@/components/ui/button";
+// import Link from "next/link";
+// import Thumbnail from "@/components/thumbnail";
+
+// interface itemType {
+//   id: number;
+//   title: string;
+//   description: string;
+//   liveLink: string;
+//   codeLink: string;
+// }
+
+// const ProjectSection = () => {
+//   return (
+//     <div>
+//       <SectionTitle
+//         intro="My Projects"
+//         title="My Latest Works"
+//         description="Welcome to my web development portfolio! Explore a collection of projects showcasing my expertise in front-end development."
+//       />
+
+//       <Carousel
+//        plugins={[
+//         Autoplay({
+//           delay: 2000,
+//         }),
+//       ]}
+//       className="w-full"
+//       >
+//         <CarouselContent className="-ml-1">
+//           {ProjectData.map((item: itemType) => (
+//             <CarouselItem key={item.id} className="pl-1 md:basis-1/2 xl:basis-1/3">
+//               <div className="p-1 h-full">
+//                 <Card className="min-h-full shadow-rn dark:shadow-dark-rn border !overflow-hidden">
+//                   <CardContent className="flex flex-col h-full space-y-4 !p-0">
+//                     <Thumbnail url={item.liveLink} className="w-full" />
+//                   </CardContent>
+//                   <CardFooter className="flex-col pt-5 space-y-5">
+//                   <h3 className="text-xl uppercase font-semibold">{item.title}</h3>
+//                     <p className="text-sm text-gray-600 text-center flex-grow">{item.description}</p>
+
+//                     {/* Ensuring buttons always stay at the bottom */}
+//                     <div className="w-full flex justify-between mt-auto">
+//                       <Button variant="link" className="px-0 py-0">
+//                         <Link target="_blank" href={item?.liveLink}>
+//                           Live View
+//                         </Link>
+//                       </Button>
+//                       <Button variant="link" className="px-0 py-0">
+//                         <Link target="_blank" href={item?.codeLink}>
+//                           Code Link
+//                         </Link>
+//                       </Button>
+//                     </div>
+//                   </CardFooter>
+//                 </Card>
+//               </div>
+//             </CarouselItem>
+//           ))}
+//         </CarouselContent>
+//         <CarouselPrevious />
+//         <CarouselNext />
+//       </Carousel>
+//     </div>
+//   );
+// };
+
+// export default ProjectSection;
